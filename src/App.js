a42c69c8e093cc0fd29175ffb46d22a0f921e6d4
avi9360 <savinash406@gmail.com>
2021-11-22 11:34:33 +0530

    Build Three

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import "./App.css";
+import React from "react";
 import { useState } from "react";
-import QrReader from "react-qr-reader";
+import BarcodeScannerComponent from "react-qr-barcode-scanner";
+import axios from "axios";
+import db from "./db.json";
 
 function App() {
-  const [result, setResult] = useState("No result");
-  function handleScan(data) {
-    if (data) {
-      console.log(data);
-      setResult(data);
-    }
+  const [data, setData] = useState([]);
+  function formatAMPM(date) {
+    const hours = date.getHours();
+    const ampm = hours >= 12 ? "pm" : "am";
+    return ampm;
   }
-  function handleError(err) {
-    console.error(err);
+  function SendMsg(data) {
+    const ampm = formatAMPM(new Date());
+    const homeCol = ampm === "am" ? "College" : "Home";
+    // axios
+    //   .post("http://localhost:3001/sms", {
+    //     phone: db[data][0].phone,
+    //     message: `${db[data][0].name} is on the way to ${homeCol}.`
+    //   })
+    //   .then(function (response) {
+    //     console.log(response);
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   });
+    console.log(db[data][0].name + " " + db[data][0].phone);
   }
   return (
-    <div>
-      <QrReader delay={300} onError={handleError} onScan={handleScan} style={{ width: "50%" }} />
-      <p>{result}</p>
-    </div>
+    <>
+      <BarcodeScannerComponent
+        width={500}
+        height={500}
+        delay={8}
+        torch={true}
+        onUpdate={(err, result) => {
+          if (result) {
+            setData([result.text]);
+            SendMsg(data);
+          }
+        }}
+      />
+      <p>{data}</p>
+    </>
   );
 }
 
